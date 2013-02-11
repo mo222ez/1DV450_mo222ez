d19316bb69a1f34d970a2bca0168218b82ac42fe
Mikael <micke1992@gmail.com>
2013-02-11 13:53:14 +0100

    Base
    Signed-off-by: Mikael <micke1992@gmail.com>

---
+++ ampta/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ampta::Application.config.secret_token = '6572bcaca28ce306602beca49923b5abc6f2dbdd8dc66a8aaf9d73fe78739c1397c01d0db4559c80f56021cd55cd0a10e95c64971adc5563d6cfce0960070ec4'
