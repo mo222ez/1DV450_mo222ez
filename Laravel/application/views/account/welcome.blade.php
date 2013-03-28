a474f057632d55890d6b9a360294197005129ce9
Mikael <micke1992@gmail.com>
2013-03-28 07:56:12 +0100

    Första commit
    Signed-off-by: Mikael <micke1992@gmail.com>

---
+++ Laravel/application/views/account/welcome.blade.php
@@ -0,0 +1,2 @@
+<h1>Hello!</h1>
+<p>This is the welcome action of the account controller.</p>
---
+++ Laravel/application/views/test/index.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Laravel: A Framework For Web Artisans</title>
+	<meta name="viewport" content="width=device-width">
+	{{ HTML::style('laravel/css/style.css') }}
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<h1>Laravel</h1>
+			<h2>A Framework For Web Artisans</h2>
+
+			<p class="intro-text" style="margin-top: 45px;">
+			</p>
+		</header>
+		<div role="main" class="main">
+			<div class="home">
+				<h2>Hello there, {{ $name }} </h2>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
